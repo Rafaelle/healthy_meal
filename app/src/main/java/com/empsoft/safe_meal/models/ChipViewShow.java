e1e2a5baf82d448bb45d2ee00861e7fb1884c453
Luiza Carvalho[push] <luiza.silveira@ccc.ufcg.edu.br>
2017-03-25 12:19:48 -0300

    primeiras mudancas para usar filtros em abas
    Please enter the commit message for your changes. Lines starting

---
+++ app/src/main/java/com/empsoft/safe_meal/models/ChipViewShow.java
@@ -0,0 +1,29 @@
+package com.empsoft.safe_meal.models;
+
+/**
+ * Created by Luiza on 25/03/2017.
+ */
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.TextView;
+
+import com.empsoft.safe_meal.R;
+
+public class ChipViewShow extends FrameLayout {
+
+    public ChipViewShow(Context context) {
+        super(context);
+        initializeView(context);
+    }
+
+    private void initializeView(Context context) {
+        LayoutInflater.from(context).inflate(R.layout.chip_view_show, this);
+    }
+
+    public void displayItem(String text) {
+        ((TextView)findViewById(R.id.chipTextViewShow)).setText(text);
+    }
+
+
+}
